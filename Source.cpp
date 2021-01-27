c1279f7ecc3f45e24ffb1476085c5dd40467126e
Dashi Batodalaev <batodalaevd@gmail.com>
2021-01-27 18:54:08 +0300

    Split_hw

--- Source.cpp
+++ Source.cpp
@@ -7,39 +7,40 @@ void Print(int arr[], const int n);
 
 void main()
 {
-
 	setlocale(LC_ALL, "Rus");
-	const int n = 10;
-	int arr[n];
 	srand(time(NULL));
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	cout << "\n";
+	int* arr = new int[n];
+
+	cout << "Исходный массив:" << endl;
 	FillRand(arr, n);
 	Print(arr, n);
-
-	//Посчитаем кол-во четных и нечетных значений в исходном массиве
-	int even_count = 0; // кол-во четных
-	int odd_count = 0;  // кол-во нечетных
-
+	
+	int even_count = 0; 
+	int odd_count = 0;  
 
 	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] % 2 == 0)even_count++;
 		else odd_count++;
 	}
-	// Выделяем память для массивов:
+	
 	int* even_values = new int[even_count] {};
 	int* odd_values = new int[odd_count] {};
 
-	//Копируем значения в соответ массивы
 	for (int i = 0, i_even = 0, i_odd = 0; i < n; i++)
 	{
 		if (arr[i] % 2 == 0)even_values[i_even++] = arr[i];
 		else odd_values[i_odd++] = arr[i];
 	}
 
-	//Выводим массивы на экран
+	cout << "Массив с четными значениями:" << endl;
 	Print(even_values, even_count);
-	Print(odd_values, odd_count);
 
+	cout<<"Массив с нечетными значениями:"<<endl;
+	Print(odd_values, odd_count);
 
 	delete[]even_values;
 	delete[]odd_values;
@@ -47,17 +48,15 @@ void main()
 
 void FillRand(int arr[], const int n)
 {
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 10;
-	}
+	for (int i = 0; i < n; i++)	arr[i] = rand() % 10;
 }
+
 void Print(int arr[], const int n)
 {
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-
-	}
-	cout << endl;
+	for (int i = 0; i < n; i++)	cout << arr[i] << "\t";
+	cout <<"\n"<< endl;
 }
+
+// Предмет: Основы программирования на языке С++
+// Преподаватель: Ковтун Олег
+// Выполнил: Батодалаев Даши
